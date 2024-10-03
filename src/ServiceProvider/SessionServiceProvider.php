26f2c0fffe4166c5e172d1206ecba0033ef62285
omegacms <omegacms@outlook.com>
2024-10-03 11:55:06 +0200

    Update facade use for reflect new structure

--- src/ServiceProvider/SessionServiceProvider.php
+++ src/ServiceProvider/SessionServiceProvider.php
@@ -24,7 +24,7 @@ namespace Omega\Session\ServiceProvider;
 use Omega\Application\Application;
 use Omega\Container\ServiceProvider\ServiceProviderInterface;
 use Omega\Session\Factory\SessionFactory;
-use Omega\Support\Facades\Config;
+use Omega\Support\Facade\Facades\Config;
 
 /**
  * SessionServiceProvider class.
